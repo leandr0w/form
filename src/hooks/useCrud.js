ac8d5f23667e70b494a5c3354dac440a85bcdfc1
leandro wasinger <leowasingerz@gmail.com>
2023-01-03 03:22:23 -0300

    resolved

--- src/hooks/useCrud.js
+++ src/hooks/useCrud.js
@@ -13,21 +13,21 @@ const useCrud = () => {
       }
     
       const createNewUser = data => {
-        const URL = 'http://users-crud.academlo.tech/users/'
+        const URL = 'https://users-crud.academlo.tech/users/'
         axios.post(URL, data)
         .then(() => getAllUsers())
         .catch(err => console.log(err))
       }
     
       const deleteUserById = id => {
-        const URL = `http://users-crud.academlo.tech/users/${id}/`
+        const URL = `https://users-crud.academlo.tech/users/${id}/`
         axios.delete(URL)
         .then(() => getAllUsers())
         .catch(err => console.log(err))
         }
     
         const updateUserById = (id, data) => {
-          const URL = `http://users-crud.academlo.tech/users/${id}/`
+          const URL = `https://users-crud.academlo.tech/users/${id}/`
           axios.put(URL, data)
           .then(res => getAllUsers())
           .catch(err => console.log(err))
